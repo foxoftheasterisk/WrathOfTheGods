e4c70a95b6631eef2af5fd212463c3cf50aa7d36
Steven Hack <foxoftheasterisk@gmail.com>
2017-09-06 21:44:08 -0700

    oh my god what did i do
    merges everywhere
    this was a bad idea

---
+++ WotgXMLExtension/EditableCity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+
+namespace WrathOfTheGods.XMLLibrary.EditingExtension
+{
+
+    public class EditableCity : City
+    {
+        public EditableCity() : base()
+        { }
+
+        public EditableCity(City city) : base(city)
+        { }
+
+        public EditableCity(string name, string region, Vector2 _position) : base (name, region, _position)
+        { }
+
+        public City Base()
+        {
+            return new City(this);
+        }
+
+        public void AddNeighbor(City neighbor)
+        {
+            neighbors.Add(neighbor);
+        }
+
+        public void RemoveNeighbor(City neighbor)
+        {
+            neighbors.Remove(neighbor);
+        }
+
+        public void Move(Vector2 vector)
+        {
+            position += vector;
+        }
+    }
+}
